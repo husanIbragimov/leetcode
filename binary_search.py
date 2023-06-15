7013d423c834b314f842b4ce6db2de53ee3a346e
husanibragimov <xusanibragimov304@gmail.com>
2023-06-15 14:25:27 +0500

    move to easy file

--- binary_search.py
+++ binary_search.py
@@ -194,6 +194,6 @@ def intersect(nums1, nums2):
     return result
 
 
-print(intersect([4, 9, 5], [9, 4, 9, 8, 4]))
-print(intersect([1, 2, 2, 1], [2, 2]))
+# print(intersect([4, 9, 5], [9, 4, 9, 8, 4]))
+# print(intersect([1, 2, 2, 1], [2, 2]))
 
